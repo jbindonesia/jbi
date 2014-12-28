4aadeec3d7a5405958d0b7c2db506792849e7d11
Abimanyu Yusuf <abim@indobit.local>
2014-12-28 22:41:43 +0700

    branch modul

--- engine/branch/menu-admin.php
+++ engine/branch/menu-admin.php
@@ -6,16 +6,10 @@
 * @license      http://creativecommons.org/licenses/by-nc/3.0/ CC BY-NC 3.0
 * @copyright    2015
 * @since        File available since 5.0
-* @category     Module Active
+* @category     Menu Themes
 */
 
-$module_active = [
-	'LC' => 'lc',
-	'Branch' => 'branch',
-	'Costumer' => 'customer'
-];
-
-foreach ($module_active as $module) {
-    include c_MODULE.$module."/menu-admin.php";
-}
+echo "
+<li class=\"branch\"><a href=\"".c_URL.$ModuleDir."branch/add\"><i class=\"fa fa-building\" aria-hidden=\"true\"></i><span>Kantor Cabang</span></a></li>
+";
 ?>
